b55feaf8e91e16ecf45f266ac6086ee9b8d9f613
yiqianyan <2378029975@qq.com>
2020-11-11 19:53:17 +0800

    测试jenkins

---
+++ src/main/java/com/example/demojenkins/DemoJenkinsApplication.java
@@ -0,0 +1,22 @@
+package com.example.demojenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+@ServletComponentScan
+public class DemoJenkinsApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemoJenkinsApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJenkinsApplication.class, args);
+    }
+
+}
